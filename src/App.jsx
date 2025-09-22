5f7dbf984cc9f8c5aafd12f337e400e37ef6aeb1
marwa youssef <marwayoussef678@gmail.com>
2025-09-22 12:21:55 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import toast, { Toaster } from 'react-hot-toast';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Toaster />
+    </>
+  )
+}
+
+export default App
