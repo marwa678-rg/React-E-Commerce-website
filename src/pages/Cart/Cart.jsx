98a7f787fa58284167679fea72fe1aa5d9f334d5
marwa youssef <marwayoussef678@gmail.com>
2025-09-28 18:08:05 +0300

    update cart component,cartcss ,and cartSlice

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,44 +1,49 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from"axios";
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegPlusSquare } from "react-icons/fa";
 import { FaRegMinusSquare } from "react-icons/fa";
-import { LiaTrashSolid } from "react-icons/lia";
 import'./cart.css';
-
+import { decermentQuantity, incrementQuantity, removeFromCart } from '../../store/slices/cartSlice/cartSlice';
 
 export const Cart = () => {
 const {items} = useSelector((state)=>state.cart);
 const[products,setProducts]=useState([]);
-
+const dispatch=useDispatch();
+const{loading,error}= useSelector(state=>state.products);
 
 
 //any async function it return  arrray of promise to solve this by promise.all that take array and turned it from pending to fullfil
 async function getProducts(){
+  try {
+            
+        const newProducts = await Promise.all( items.map(async (item)=>{
+        const response = await axios.get(`https://dummyjson.com/products/${item.id}`);
 
-const newProducts = await Promise.all( items.map(async (item)=>{
-const response = await axios.get(`https://dummyjson.com/products/${item.id}`);
+        return {...response.data,quantity:item.quantity,id:item.id};
 
-return {...response.data,quantity:item.quantity};
-
-})
-)
-
-
-setProducts(newProducts)
+        })
+        );
 
 
+        setProducts(newProducts);
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
 
 }
-useEffect(()=>{
-  getProducts()
-},[items]);
+      useEffect(()=>{
+        getProducts()
+      },[items]);
 
-function CartProduct({thumbnail, title,price,brand,quantity}){
+function CartProduct({thumbnail, title,price,brand,quantity,id}){
 
   return(
  
@@ -52,25 +57,39 @@ function CartProduct({thumbnail, title,price,brand,quantity}){
       <p>brand: {brand}</p>
       <p>price:  ${Math.round(price)}</p>
     </div>
+
+
+    
   <div className="quantity-container">
-      <span> <FaRegPlusSquare  className="inc" onClick={()=>{
-          console.log(1)
+
+
+      <FaRegPlusSquare  style={{fontSize:"30px",cursor:"pointer"}} className='inc' onClick={()=>{
+         dispatch(incrementQuantity ({id}));
       }}/>
-      {quantity}
-      <FaRegMinusSquare className='dec' onClick={()=>{
-        console.log(-1)
+
+
+     <span>{quantity}</span>
+
+      <FaRegMinusSquare  style={{fontSize:"30px",cursor:"pointer"}} className='dec'  onClick={()=>{
+        dispatch(decermentQuantity ({id}));
       }}/> 
 
-      </span>
+      
 
-      {/* <LiaTrashSolid /> */}
+    </div>
+
+    <div className="remove-btn">
+      <button className="remove-btn" onClick={()=>{
+        dispatch(removeFromCart({id}))
+      }}> 🗑️</button>
     </div>
 
     </div>
  
   )
 }
-
+const totalPrice = products.reduce((sum,item)=>sum + item.price * item.quantity,0);
+console.log(totalPrice);
   return (
     <div className='cart-container'>
     <h1>My Cart</h1>
@@ -89,16 +108,17 @@ function CartProduct({thumbnail, title,price,brand,quantity}){
         brand={item.brand}
         price={item.price}
         key={item.id}
+        id={item.id}
         quantity={item.quantity}
         />
 
       ))}
           
 
-    </div>
+     </div>
       
     {products.length > 0 && <div>
-      <h1>Total Price = {Math.round()}</h1>
+      <h1>Total Price =$ {totalPrice}</h1>
       </div>}
     
     
--- src/store/slices/cartSlice/cartSlice.jsx
+++ src/store/slices/cartSlice/cartSlice.jsx
@@ -23,13 +23,29 @@ const cartSlice = createSlice({
      }
      console.log(itemIndex) 
     },
+    removeFromCart:(state,action)=>{
+      const itemIndex = state.items.findIndex((item)=>item.id === action.payload.id);
+      if(itemIndex >=0){
+        state.items.splice(itemIndex,1);
+      }
+
+    },
+    incrementQuantity:(state,action)=>{
+      const itemIndex =state.items.findIndex((item)=>item.id ===action.payload.id);
+      if(itemIndex>=0){
+        state.items[itemIndex].quantity +=1;
+      }
+   },
+decermentQuantity:(state,action)=>{
+  const itemIndex =state.items.findIndex((item)=>item.id === action.payload.id);
+  if(itemIndex >=0 && state.items[itemIndex].quantity >1){
+    state.items[itemIndex].quantity -=1;
+  }
+
+},
 
 
-
-
-
-    
   }
 })
- export const{addToCart} = cartSlice.actions;
+ export const{addToCart,removeFromCart,incrementQuantity,decermentQuantity} = cartSlice.actions;
  export default cartSlice.reducer;
